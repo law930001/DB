e3b7a3ff403b3fb081b9225b1e653ace8f5d455f
law930001 <law930001@gmail.com>
2021-07-05 12:46:04 +0000

    0705

--- tools/ic15_eval.sh
+++ tools/ic15_eval.sh
@@ -1,5 +1,6 @@
 cd ../
-python eval.py experiments/seg_detector/ic15_hrnet48_thre.yaml \
---resume model/model_epoch_3000_minibatch_375000 \
+CUDA_VISIBLE_DEVICES=2 \
+python eval.py experiments/seg_detector/ic15_resnet50_deform_thre.yaml \
+--resume /root/Storage/DB/workspace/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model/model_epoch_1200 \
 --polygon \
---box_thresh 0.6
+--box_thresh 0.7
--- tools/ic15_train.sh
+++ tools/ic15_train.sh
@@ -1,7 +1,6 @@
 cd ../
 CUDA_VISIBLE_DEVICES=2 \
-python train.py experiments/seg_detector/ic15_hrnet48_thre.yaml \
+python train.py experiments/seg_detector/ic15_resnet50_deform_thre.yaml \
 --num_gpus 1 \
 --num_workers 10 \
---batch_size 8 \
---resume outputs/workspace/DB/SegDetectorModel-seg_detector/hrnet48/L1BalanceCELoss/model_TBrain/model_epoch_1_minibatch_264000
+--batch_size 24
