ca4cdf1ac7916b0e485ed88f75ea0696079ed483
law930001 <law930001@gmail.com>
2021-08-09 07:02:50 +0000

    version rename

--- tools/ic15_eval.sh
+++ tools/ic15_eval.sh
@@ -1,7 +1,7 @@
 cd ../
 CUDA_VISIBLE_DEVICES=0 \
-python eval.py experiments/seg_detector/tt_efficient_thre.yaml \
---resume /root/Storage/DB_v100/model/5004/final \
+python eval.py experiments/seg_detector/ic17_hrnet48_thre.yaml \
+--resume /root/Storage/DB_v100/model/5002/model_epoch_1123_minibatch_1152000 \
 --polygon \
 --box_thresh 0.7
 # --thresh 0.3
